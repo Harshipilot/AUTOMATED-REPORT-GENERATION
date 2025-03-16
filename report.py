f8602a010308d74878b364a7893d20fa26fe3c70
Harshipilot <harshgowda751@gmail.com>
2025-03-16 10:10:29 +0530

    Add files via upload

---
+++ report.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+def analyze(file_path):
+    df=pd.read_csv(file_path)
+    summary=df.describe().T
+    return summary
+def report(data_summary,output_pdf="report_reportlab.pdf"):
+    doc=SimpleDocTemplate(output_pdf,pagesize=letter)
+    elements=[]
+    data = [["Metric"]+list(data_summary.columns)]  
+    for index,row in data_summary.iterrows():
+        data.append([index]+[f"{val:.2f}"for val in row])
+    table=Table(data)
+    style=TableStyle([
+        ("BACKGROUND",(0,0),(-1,0),colors.grey),
+        ("TEXTCOLOR",(0,0),(-1,0),colors.whitesmoke),
+        ("ALIGN",(0,0),(-1,-1),"CENTER"),
+        ("GRID",(0,0),(-1,-1),1,colors.black),
+    ])
+    table.setStyle(style)
+    elements.append(table)
+    doc.build(elements)
+    print(f"Report generated:{output_pdf}")
+file_path="sample_data.csv"
+summary=analyze(file_path)
+report(summary)
